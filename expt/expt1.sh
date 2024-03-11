002a3ac14835e3861f50236849eb3ef0d80096d7
masahiro-negishi <negishi-masahiro1110@g.ecc.u-tokyo.ac.jp>
2024-03-11 17:33:42 +0100

    sh for expt 1

---
+++ expt/expt1.sh
@@ -0,0 +1,22 @@
+cd $(dirname $0)
+cd ../
+
+MAX_JOBS=5
+
+for depth in 3 4 5
+do
+    for lr in 0.0001 0.001 0.01
+    do
+        for margin in 0.01 0.1 1 10 100
+        do
+            while (($(jobs -r | wc -l) >= $MAX_JOBS)); do
+                sleep 1
+            done
+            python src/train.py --dataset_name "MUTAG" --depth $depth --loss_name "triplet" \
+            --batch_size "20" --n_epochs 1000 --lr $lr --save_interval 100 --seed 0 \
+            --margin $margin &
+        done
+    done
+done
+
+wait
