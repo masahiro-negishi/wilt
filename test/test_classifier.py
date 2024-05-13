81833c25ac25921ed1ccc2000aa402c10389f0cf
masahiro-negishi <negishi-masahiro1110@g.ecc.u-tokyo.ac.jp>
2024-05-13 10:59:38 +0200

    add knc

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -5,8 +5,8 @@ import torch
 from torch_geometric.datasets import TUDataset  # type: ignore
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
+from classifier import knc, svm  # type: ignore
 from path import DATA_DIR  # type: ignore
-from svm import svm  # type: ignore
 
 
 def test_svm(fixture_prepare_distances):
@@ -21,4 +21,19 @@ def test_svm(fixture_prepare_distances):
         indices[len(data) * 3 // 4 :],
         distances,
         1,
+    )
+
+
+def test_knc(fixture_prepare_distances):
+    dataset_name, _, path = fixture_prepare_distances
+    data = TUDataset(root=os.path.join(DATA_DIR, "TUDataset"), name=dataset_name)
+    distances = torch.load(path)
+    indices = list(range(len(data)))
+    knc(
+        data,
+        indices[: len(data) // 2],
+        indices[len(data) // 2 : len(data) * 3 // 4],
+        indices[len(data) * 3 // 4 :],
+        distances,
+        5,
     )
