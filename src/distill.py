c7bf1f3ef803491a09d2b5492639bb356473cd5f
masahiro-negishi <negishi-masahiro1110@g.ecc.u-tokyo.ac.jp>
2024-07-07 10:12:01 +0200

    add new datasets

--- src/distill.py
+++ src/distill.py
@@ -12,8 +12,8 @@ from scipy.optimize import nnls  # type: ignore
 from torch import nn
 from torch.optim import Adam
 from torch.utils.data import BatchSampler
-from torch_geometric.data import Dataset  # type: ignore
-from torch_geometric.datasets import ZINC, TUDataset  # type: ignore
+from torch_geometric.data import Data, Dataset  # type: ignore
+from torch_geometric.datasets import ZINC, MoleculeNet, TUDataset  # type: ignore
 
 from path import DATA_DIR, GNN_DIR, RESULT_DIR  # type: ignore
 from tree import WeisfeilerLemanLabelingTree
@@ -328,12 +328,34 @@ def train_wrapper(
         **kwargs: additional arguments
     """
 
-    if dataset_name in ["synthetic_cls", "synthetic_reg"]:
+    if dataset_name in ["synthetic_bin", "synthetic_mul", "synthetic_reg"]:
         data = torch.load(
             os.path.join(DATA_DIR, "synthetic", dataset_name[-3:], "dataset.pt")
         )
     elif dataset_name == "ZINC":
         data = ZINC(root=os.path.join(DATA_DIR, "ZINC"), subset=True, split="train")
+    elif dataset_name in ["Lipo", "ESOL"]:
+        data = MoleculeNet(root=os.path.join(DATA_DIR, dataset_name), name=dataset_name)
+        converted = []
+        xdict = {}
+        edict = {}
+        for d in data:
+            newx = torch.zeros(len(d.x), 1)
+            for i, x in enumerate(d.x):
+                idx = tuple(x.tolist())
+                if idx not in xdict:
+                    xdict[idx] = len(xdict)
+                newx[i][0] = xdict[idx]
+            newe = torch.zeros(len(d.edge_attr), 1)
+            for i, e in enumerate(d.edge_attr):
+                idx = tuple(e.tolist())
+                if idx not in edict:
+                    edict[idx] = len(edict)
+                newe[i][0] = edict[idx]
+            converted.append(
+                Data(x=newx, edge_attr=newe, edge_index=d.edge_index, y=d.y)
+            )
+        data = converted
     else:
         data = TUDataset(root=os.path.join(DATA_DIR, "TUDataset"), name=dataset_name)
     tree_start = time.time()
@@ -411,9 +433,13 @@ if __name__ == "__main__":
             "MUTAG",
             "Mutagenicity",
             "NCI1",
-            "synthetic_cls",
+            "ENZYMES",
+            "synthetic_bin",
+            "synthetic_mul",
             "synthetic_reg",
             "ZINC",
+            "Lipo",
+            "ESOL",
         ],
     )
     parser.add_argument(
